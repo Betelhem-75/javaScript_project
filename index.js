5e637b282e1e5d5f1b0324cc20b5a1e86be59eea
betelhem-75 <betelhemfikadu75gmail.com>
2025-05-17 08:11:27 -0500

    your describ

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const moviesListsEL = document.querySelector(' .movies__lists');
 
 async function searchChange(event) {
   const Title = event.target.value
-  const movies = await fetch(`https://omdbapi.com?apikey=1a73e81b&s=${Title}`);
-  moviesListsEL.innerHTML = moviesData.Search
+  const movies = await fetch(`https://omdbapi.com/?s=fast&apikey=1a73e81bTitle=$(Title)`);
+  const moviesData = await movies.json();
+  moviesListsEL.innerHTML = moviesData.Search.slice(0, 5)
   .map(
     (movie) => 
       `<div class="movies__list">
           <div class="movies__lists--container">
-            <img src="${movies.Poster}" class="poster" alt="" />
-            <h2 class="movie__title"> ${movies.Title} </h2>
-            <p class="movie__type"><b>Year:</b> ${movies.Year}</p>
-            <p class="movie__type"><b>imdbID:</b> ${movies.imdbID}</p>
-            <p class="movie__type"><b>Type:</b> ${movies.Type}</p>
+            <img src="${movie.Poster}" class="poster" alt="" />
+            <h2 class="movie__title"> ${movie.Title} </h2>
+            <p class="movie__type"><b>Year:</b> ${movie.Year}</p>
+            <p class="movie__type"><b>imdbID:</b> ${movie.imdbID}</p>
+            <p class="movie__type"><b>Type:</b> ${movie.Type}</p>
            
          </div>
      </div>` 
@@ -21,7 +22,7 @@ async function searchChange(event) {
 }
 
 async function moviesMain() {
-  const movies = await fetch(`https://omdbapi.com?apikey=1a73e81b&s=${Title}`);
+  const movies = await fetch(`https://omdbapi.com/?s=fast&apikey=1a73e81b`);
   const moviesData = await movies.json();
   console.log(moviesData);
   moviesListsEL.innerHTML = moviesData.Search
