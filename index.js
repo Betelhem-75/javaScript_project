85f656f820d417af5f88a4976d5bb37a74d07b0b
betelhem-75 <betelhemfikadu75gmail.com>
2025-05-22 14:53:56 -0500

    your describ

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
-const moviesListsEL = document.querySelector(' .movies__lists');
+const moviesListsEL = document.querySelector(" .movies__lists");
+const searchInput = document.getElementById("search");
 
+document.classList += ' books__loading'
+searchInput.addEventListener('keyup', e => {
+  let currentValue = e.target.value.toLowerCase();
+  console.log(currentValue);
+  let movies = document.querySelectorAll('h2.movie__title');
+  movies.forEach(movie => {
+    if (movie.textContent.toLowerCase().includes(currentValue)){
+      movie.parentNode.parentNode.style.display = 'block';
+    }
+    else{
+      movie.parentNode.parentNode.style.display = 'none';
+    }
+  });
+})
+
+
+async function moviesmain() {
+  const movies = await fetch("https://omdbapi.com/?s=fast&apikey=1a73e81b")
+  const moviesData = await movies.json();
+  
+  moviesListsEL.innerHTML = moviesData.Search
+  .map(
+    (movie) => 
+      `<div class="movies__list">
+          <div class="movies__lists--container">
+            <img src="${movie.Poster}" class="poster" alt="" />
+            <h2 class="movie__title"> ${movie.Title} </h2>
+            <p class="movie__type"><b>Year:</b> ${movie.Year}</p>
+            <p class="movie__type"><b>imdbID:</b> ${movie.imdbID}</p>
+            <p class="movie__type"><b>Type:</b> ${movie.Type}</p>
+         </div>
+      </div>` 
+   )   
+ .join("");
+  
+
+} 
+moviesmain();
+
+
+/*
 async function searchChange(event) {
   const Title = event.target.value
   const movies = await fetch(`https://omdbapi.com/?s=fast&apikey=1a73e81bTitle=$(Title)`);
   const moviesData = await movies.json();
-  moviesListsEL.innerHTML = moviesData.Search.slice(0, 5)
+  moviesListsEL.innerHTML = moviesData.Search
   .map(
     (movie) => 
       `<div class="movies__list">
@@ -44,3 +86,4 @@ async function moviesMain() {
 }
 
 moviesMain();
+*/
